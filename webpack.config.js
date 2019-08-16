59f9451b5ce0d802005653d10474cb71de029ab1
liebhartdevelopment <nathaniel95975@gmail.com>
2019-08-16 09:19:32 -0500

    webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: {
-    app: [
-      '@babel/polyfill',
-      './src/app.js',
-    ],
+    app: ["@babel/polyfill", "./src/app.js"]
   },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'app.bundle.js',
+    path: path.resolve(__dirname, "build"),
+    filename: "app.bundle.js"
   },
+  mode: "none",
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.js?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         query: {
-           presets: ['@babel/preset-env', '@babel/preset-stage-0']
+          presets: ["@babel/preset-env"]
         }
-    }]
+      }
+    ]
   }
-}
+};
